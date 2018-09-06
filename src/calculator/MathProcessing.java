a996840046dafb09508f39771c54c66c55e8877b
kthong3 <kthongsakounh@gmail.com>
2018-09-06 10:50:27 -0500

    add MathProcessing interface

---
+++ src/calculator/MathProcessing.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public interface MathProcessing {
+    String SEPARATOR = " ";
+    String getKeyword(); // add
+    char getSymbol(); // +
+    double doCalculation(double leftVal, double rightVal);
+}
